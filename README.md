8e0febac0ee6aa43559a457944511714b109afa6
Z-Dante <34379513+Z-Dante@users.noreply.github.com>
2022-02-17 19:54:40 +0600

    Working scripts and FPS changer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SVPlite
+# SVPlitePlus - Based on https://github.com/Nuihc88/SVPlite
 SVPlite - Realtime-Optimized AviSynth+ Script-Templates for the SmoothVideo Project's SVPflow-filters.
 
 Project Goal: Making SVPflow libraries more usable and accessible on older computers and portable video players.
---
+++ SVPLiteSetFPS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SVPLiteSetFPS
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        { 
+            Console.Write("Input Desired Framerate: ");
+            var fps = Console.ReadLine();
+            foreach (string file in Directory.EnumerateFiles(".", "*.avs"))
+            {
+                string contents = File.ReadAllText(file);
+                contents = Regex.Replace(contents, "{num:[0-9]*,", "{num:" + fps + ",");
+                File.WriteAllText(file, contents);
+            }
+            Console.WriteLine("Done!");
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
